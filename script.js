f47b030c4074216845cf2e85354bb8656a70285f
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-21 10:44:41 +0300

    brokens keys

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+function brokenKeys(text, expectedoutput) {
+  const brokenKeys = [];
+
+  for (let i = 0; i < expectedoutput.length; i++) {
+    if (text[i] !== expectedoutput[i] && !brokenKeys.includes(expectedoutput[i])) {
+      brokenKeys.push(expectedoutput[i]);
+    }
+  }
+
+  return brokenKeys;
+}
+
+const Text = "gppd  night";
+const expectedoutput = "good ";
+const result = brokenKeys(Text, expectedoutput);
+console.log(result);
